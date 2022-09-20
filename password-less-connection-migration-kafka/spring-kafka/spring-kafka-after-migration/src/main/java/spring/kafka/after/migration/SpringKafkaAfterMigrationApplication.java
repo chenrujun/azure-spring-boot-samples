125b24ec67842294065b98b26504e67681289f01
Rujun Chen <rujche@microsoft.com>
2022-09-20 18:07:36 +0800

    Make SpringKafkaAfterMigrationApplication same to SpringKafkaBeforeMigrationApplication

--- password-less-connection-migration-kafka/spring-kafka/spring-kafka-after-migration/src/main/java/spring/kafka/after/migration/SpringKafkaAfterMigrationApplication.java
+++ password-less-connection-migration-kafka/spring-kafka/spring-kafka-after-migration/src/main/java/spring/kafka/after/migration/SpringKafkaAfterMigrationApplication.java
@@ -1,14 +1,43 @@
 package spring.kafka.after.migration;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class SpringKafkaAfterMigrationApplication {
 
+    private static final String TOPIC_NAME = "SpringKafkaBeforeMigrationApplicationTopic";
+
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SpringKafkaAfterMigrationApplication.class);
-        context.getBean(Sender.class).send("kafka-spring-before-migration-send-key", "kafka-spring-before-migration-send-data");
+        SpringApplication.run(SpringKafkaAfterMigrationApplication.class, args);
+    }
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name(TOPIC_NAME)
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+
+    @KafkaListener(id = "myId", topics = TOPIC_NAME)
+    public void listen(String in) {
+        System.out.println("Received message: " + in);
+    }
+
+    @Bean
+    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+        return args -> {
+            String message = "test data 111";
+            System.out.println("Sending message: " + message);
+            template.send(TOPIC_NAME, message);
+        };
     }
 }
