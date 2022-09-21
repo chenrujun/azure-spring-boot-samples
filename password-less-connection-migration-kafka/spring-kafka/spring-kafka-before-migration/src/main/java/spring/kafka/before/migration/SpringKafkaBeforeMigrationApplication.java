6e1f9f3a71de2e43f336c058a65f53c94554a3af
Rujun Chen <rujche@microsoft.com>
2022-09-21 17:30:26 +0800

    Simplify SpringKafkaBeforeMigrationApplication.

--- password-less-connection-migration-kafka/spring-kafka/spring-kafka-before-migration/src/main/java/spring/kafka/before/migration/SpringKafkaBeforeMigrationApplication.java
+++ password-less-connection-migration-kafka/spring-kafka/spring-kafka-before-migration/src/main/java/spring/kafka/before/migration/SpringKafkaBeforeMigrationApplication.java
@@ -1,29 +1,21 @@
 package spring.kafka.before.migration;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringKafkaBeforeMigrationApplication {
 
-    private static final String TOPIC_NAME = "SpringKafkaBeforeMigrationApplicationTopic";
+    private static final String TOPIC_NAME = "kafka-spring-before-migration-topic";
 
     public static void main(String[] args) {
         SpringApplication.run(SpringKafkaBeforeMigrationApplication.class, args);
-    }
-
-    @Bean
-    public NewTopic topic() {
-        return TopicBuilder.name(TOPIC_NAME)
-                .partitions(10)
-                .replicas(1)
-                .build();
     }
 
     @KafkaListener(id = "myId", topics = TOPIC_NAME)
@@ -34,9 +26,9 @@ public class SpringKafkaBeforeMigrationApplication {
     @Bean
     public ApplicationRunner runner(KafkaTemplate<String, String> template) {
         return args -> {
-            String message = "test data 111";
-            System.out.println("Sending message: " + message);
-            template.send(TOPIC_NAME, message);
+                String message = "test data. date = " + new Date();
+                System.out.println("Sending message: " + message);
+                template.send(TOPIC_NAME, message);
         };
     }
 }
