5c44316b3b7cad06eaac1a271f58f7221dc85027
Rujun Chen <rujche@microsoft.com>
2022-09-22 13:58:34 +0800

    Create spring-cloud-stream-binder-kafka-after-migration according to spring-cloud-stream-binder-kafka-before-migration

---
+++ password-less-connection-migration-kafka/spring-cloud-stream-binder-kafka/spring-cloud-stream-binder-kafka-after-migration/src/main/java/spring/cloud/stream/binder/kafka/after/migration/KafkaProducer.java
@@ -0,0 +1,22 @@
+package spring.cloud.stream.binder.kafka.after.migration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class KafkaProducer {
+
+    @Autowired
+    private StreamBridge streamBridge;
+
+    @Scheduled(cron = "*/2 * * * * *")
+    public void sendMessage(){
+        String data = "test message. date = " + new Date();
+        System.out.println("Sending message by streamBridge. data = " + data);
+        streamBridge.send("producer-out-0", data);
+    }
+}
---
+++ password-less-connection-migration-kafka/spring-cloud-stream-binder-kafka/spring-cloud-stream-binder-kafka-after-migration/src/main/java/spring/cloud/stream/binder/kafka/after/migration/SpringCloudStreamBinderKafkaAfterMigrationApplication.java
@@ -0,0 +1,31 @@
+package spring.cloud.stream.binder.kafka.after.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@SpringBootApplication
+public class SpringCloudStreamBinderKafkaAfterMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamBinderKafkaAfterMigrationApplication.class);
+    }
+
+    @Bean
+    public Supplier<String> producer() {
+        return () -> {
+            String data = "Test data by producer. date = " + new Date();
+            System.out.println("Producing string. data = " + data);
+            return data;
+        };
+    }
+
+    @Bean
+    public Consumer<String> consumer() {
+        return message -> System.out.println("Consuming message. message = " + message);
+    }
+}
